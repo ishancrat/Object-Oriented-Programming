d43c609bd8a28ecbffb1fae7405775c1cc64716d
Ishan Bajpai <82746770+ishancrat@users.noreply.github.com>
2023-08-18 22:50:14 +0530

    Create globalVars.h

---
+++ Lab2/globalVars.h
@@ -0,0 +1,21 @@
+/*
+ * globalVars.h
+ *
+ *  Created on: 18-Aug-2023
+ *      Author: Ishan
+ */
+
+#ifndef GLOBALVARS_H_
+#define GLOBALVARS_H_
+
+
+static int successfulTransactions=0;
+static int failedTransactions=0;
+static int totalBooks=0;
+
+static int successfulTransactionsPtr=0;
+static int failedTransactionsPtr=0;
+static int totalBooksPtr=0;
+
+
+#endif /* GLOBALVARS_H_ */
